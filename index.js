44f433856853ede5eec74ea96c2731ece9b45843
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-03-12 01:10:36 +0000

    AutomatedCommitAndPush

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(rew, res){
-    res.render("hello, world");
+    res.send("hello, world");
 });
 
 app.get("/*", function(rew, res){
-    res.render("page not found");
+    res.send("page not found");
 });
 
 app.listen(process.env.PORT, function(){
